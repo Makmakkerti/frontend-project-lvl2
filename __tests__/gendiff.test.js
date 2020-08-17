07ce25a344ddf720306256fcb2f6bbd542ede871
makmakkerti <makmakkerti007@gmail.com>
2020-08-17 13:32:37 +0300

    CI settings enabled

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,13 @@
-/* eslint-disable no-undef */
 import checkDiff from '../index';
 import { readFile } from '../src/parsers';
 
-const result = readFile('./fixtures/result.txt');
-test.each([
+const tests = [
   ['fixtures/before.json', 'fixtures/after.json'],
   ['fixtures/before.yml', 'fixtures/after.yml'],
   ['fixtures/before.ini', 'fixtures/after.ini'],
-])('Test checkDiff', (a, b) => {
+];
+const result = readFile('./fixtures/result.txt');
+
+test.each(tests)('Test checkDiff', (a, b) => {
   expect(checkDiff(a, b)).toBe(result);
 });
