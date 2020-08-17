3ec49273d6b1f7fc653db6b9f010b8ae900abe7c
makmakkerti <makmakkerti007@gmail.com>
2020-08-17 16:08:02 +0300

    Tests each string interpolation usage added

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,15 @@
 import checkDiff from '../index';
 import { readFile } from '../src/parsers';
 
-const tests = [
-  ['fixtures/before.json', 'fixtures/after.json'],
-  ['fixtures/before.yml', 'fixtures/after.yml'],
-  ['fixtures/before.ini', 'fixtures/after.ini'],
-];
-const result = readFile('./fixtures/result.txt');
+const testing = ({ before, after, result }) => {
+  const received = checkDiff(before, after);
+  const expected = readFile(result);
+  expect(received).toBe(expected);
+};
 
-test.each(tests)('Test checkDiff', (a, b) => {
-  expect(checkDiff(a, b)).toBe(result);
-});
+test.each`
+  before                          | after                     | result
+  ${'./fixtures/before.json'}     | ${'./fixtures/after.json'}|  ${'./fixtures/result.txt'}
+  ${'./fixtures/before.yml'}      | ${'./fixtures/after.yml'} |  ${'./fixtures/result.txt'}
+  ${'./fixtures/before.ini'}      | ${'./fixtures/after.ini'} |  ${'./fixtures/result.txt'}
+  `('Check each file format', testing);
