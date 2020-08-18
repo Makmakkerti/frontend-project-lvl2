db803d4db3d7ff9e621be5b49ede6f6318e87d3b
makmakkerti <makmakkerti007@gmail.com>
2020-08-18 16:00:09 +0300

    Step 7 completed

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,10 +6,15 @@ const testing = ({ before, after, result }) => {
   const expected = readFile(result);
   expect(received).toBe(expected);
 };
+const fpath = './fixtures/flat/';
+const npath = './fixtures/nested/';
 
 test.each`
-  before                          | after                     | result
-  ${'./fixtures/before.json'}     | ${'./fixtures/after.json'}|  ${'./fixtures/result.txt'}
-  ${'./fixtures/before.yml'}      | ${'./fixtures/after.yml'} |  ${'./fixtures/result.txt'}
-  ${'./fixtures/before.ini'}      | ${'./fixtures/after.ini'} |  ${'./fixtures/result.txt'}
+  before                       | after                     | result
+  ${`${fpath}before.json`}     | ${`${fpath}after.json`}   |  ${`${fpath}result.txt`}
+  ${`${fpath}before.yml`}      | ${`${fpath}after.yml`}    |  ${`${fpath}result.txt`}
+  ${`${fpath}before.ini`}      | ${`${fpath}after.ini`}    |  ${`${fpath}result.txt`}
+  ${`${npath}before.json`}     | ${`${npath}after.json`}   |  ${`${npath}result.txt`}
+  ${`${npath}before.yml`}      | ${`${npath}after.yml`}    |  ${`${npath}result.txt`}
+  ${`${npath}before.ini`}      | ${`${npath}after.ini`}    |  ${`${npath}result.txt`}
   `('Check each file format', testing);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
-/* eslint-disable no-restricted-syntax */
 import _ from 'lodash';
 import { parseFile } from './parsers.js';
+import stylish from './stylish.js';
 
-const buildDiff = (obj1, obj2, depth = 2) => {
-  const result = [];
-  const tabs = ' '.repeat(depth);
-  const signs = { no: '  ', add: '+ ', rem: '- ' };
-
-  result.push('{');
-  for (const [key, value] of Object.entries(obj1)) {
-    if (_.has(obj2, key)) {
-      if (obj2[key] === value) {
-        result.push(`${tabs}${signs.no}${key}: ${value}`);
-      } else {
-        result.push(`${tabs}${signs.rem}${key}: ${value}`);
-        result.push(`${tabs}${signs.add}${key}: ${obj2[key]}`);
-      }
-    } else {
-      result.push(`${tabs}${signs.rem}${key}: ${value}`);
-    }
+const getTypeSettings = (key, before, after, cb) => {
+  if (!_.has(before, key) && _.has(after, key)) return { type: 'added', value: after[key] };
+  if (_.has(before, key) && !_.has(after, key)) return { type: 'deleted', value: before[key] };
+  if (_.isObject(before[key]) && _.isObject(after[key])) {
+    const children = cb(before[key], after[key]);
+    return { type: 'nested', children };
   }
+  if (before[key] !== after[key]) return { type: 'modified', oldValue: before[key], newValue: after[key] };
+  if (before[key] === after[key]) return { type: 'unmodified', value: before[key] };
+  return null;
+};
 
-  for (const [key, value] of Object.entries(obj2)) {
-    if (!(_.has(obj1, key))) {
-      result.push(`${tabs}${signs.add}${key}: ${value}`);
-    }
-  }
-  result.push('}');
-  return result.join('\n');
+const buildDiff = (before, after) => {
+  const mergedKeys = _.union(_.keys(before), _.keys(after));
+
+  return mergedKeys.sort().map((key) => {
+    const args = [key, before, after];
+    const { type } = getTypeSettings(...args, buildDiff);
+    return { key, type, ...getTypeSettings(...args, buildDiff) };
+  });
 };
 
 const checkDiff = (file1, file2) => {
   const obj1 = parseFile(file1);
   const obj2 = parseFile(file2);
-  return buildDiff(obj1, obj2);
+  return stylish(buildDiff(obj1, obj2));
 };
 
 export default checkDiff;
