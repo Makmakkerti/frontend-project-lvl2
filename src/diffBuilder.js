87e04f29a0a3f00a0fd867b5cb4f3dfb47078b09
makmakkerti <makmakkerti007@gmail.com>
2020-09-02 14:41:24 +0300

    BuildDiff: buildNodeData removed from code

--- src/diffBuilder.js
+++ src/diffBuilder.js
@@ -4,22 +4,21 @@ export const buildDiff = (oldData, newData) => {
   const mergedKeys = _.union(_.keys(oldData), _.keys(newData));
 
   return mergedKeys.sort().map((key) => {
-    const buildNodeData = () => {
-      if (!_.has(oldData, key)) {
-        return { type: 'added', value: newData[key] };
-      }
-      if (!_.has(newData, key)) {
-        return { type: 'deleted', value: oldData[key] };
-      }
-      if (_.isObject(oldData[key]) && _.isObject(newData[key])) {
-        const children = buildDiff(oldData[key], newData[key]);
-        return { type: 'nested', children };
-      }
-      if (oldData[key] !== newData[key]) {
-        return { type: 'modified', oldValue: oldData[key], value: newData[key] };
-      }
-      return { type: 'unmodified', value: oldData[key] };
-    };
-    return { key, ...buildNodeData() };
+    if (!_.has(oldData, key)) {
+      return { key, type: 'added', value: newData[key] };
+    }
+    if (!_.has(newData, key)) {
+      return { key, type: 'deleted', value: oldData[key] };
+    }
+    if (_.isObject(oldData[key]) && _.isObject(newData[key])) {
+      const children = buildDiff(oldData[key], newData[key]);
+      return { key, type: 'nested', children };
+    }
+    if (oldData[key] !== newData[key]) {
+      return {
+        key, type: 'modified', oldValue: oldData[key], value: newData[key]
+      };
+    }
+    return { key, type: 'unmodified', value: oldData[key] };
   });
 };
